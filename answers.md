8696df458370517071f1526ccc6f84c8c7194543
GavinGalusha <112968216+GavinGalusha@users.noreply.github.com>
2024-04-17 20:29:32 +0000

    new

--- answers.md
+++ answers.md
@@ -1,17 +1,29 @@
 # CMPS 2200 Recitation 10## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:**_______Gavin Galusha__________________
+**Name:**________Isaac Ratzaan_________________
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
 
+**2)**
 
-
-- **2)**
+- $W(m,n) = W(2m) + W(n) + O(1) = O(m+n)$
+-   
 
 - **4)**
 
+-  In the worst case, we select a starting node and call reachable once. It will traverse the entire graph, and if it reaches every node, then the graph is connected. If any node is not reached by this process, then the graph is not connected. Therefore, `reachable` will only be called once in all cases.
+-  
+
 - **5)**
 
+- $W(m,n) = W(2m) + W(n) +O(1) = O(m+n)$
+- 
+
 - **7)**
+
+- the work of the `reachable` function changes primarily because finding all neighbors of a node becomes a $O(n)$ operation, where $n$ is the number of nodes. You must check the entire row in the matrix to see which nodes are connected to the current node, resulting in $O(n)$ work per node, and since this check happens for every node, the total work in the worst case becomes $O(n^2)$. 
+
+
+
--- main.py
+++ main.py
@@ -1,42 +1,51 @@
 from collections import defaultdict
 
 def make_undirected_graph(edge_list):
-    """ Makes an undirected graph from a list of edge tuples. """
-    graph = defaultdict(set)
-    for e in edge_list:
-        graph[e[0]].add(e[1])
-        graph[e[1]].add(e[0])
-    return graph
+		""" Makes an undirected graph from a list of edge tuples. """
+		graph = defaultdict(set)
+		for e in edge_list:
+				graph[e[0]].add(e[1])
+				graph[e[1]].add(e[0])
+		return graph
 
 
 def reachable(graph, start_node):
-    """
-    Returns:
-      the set of nodes reachable from start_node
-    """
-    result = set([start_node])
-    frontier = set([start_node])
-    while len(frontier) != 0:
-        ###TODO
-        pass
-    return result
+		"""
+		Returns:
+			the set of nodes reachable from start_node
+		"""
+		result = set([start_node])
+		frontier = set([start_node])
+		while len(frontier) != 0:
+			 current_node = frontier.pop()  
+			 for neighbor in graph[current_node]:  
+					 if neighbor not in result:  
+							 result.add(neighbor)  
+							 frontier.add(neighbor)
+		return result
 
 
 
 
 
 def connected(graph):
-    ### TODO
-    pass
+	start_node = next(iter(graph))
+	reached = reachable(graph, start_node)
+	return len(reached) == len(graph) 
 
 
 
 
 def n_components(graph):
-    """
-    Returns:
-      the number of connected components in an undirected graph
-    """
-    ### TODO
-    pass
-
+		"""
+		Returns:
+			the number of connected components in an undirected graph
+		"""
+		visited = set()
+		count = 0
+		for node in graph:
+			if node not in visited:
+					count += 1  
+					component_nodes = reachable(graph, node)
+					visited.update(component_nodes)
+		return count
